888b34f792d9364b6c3eab51aa00617833bddf6a
jkmazzetti <jkmazzetti@outlook.com>
2022-01-27 17:16:31 -0300

    Exercise, solved.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(first, second) {
+    let result=0;
+    if(typeof(first) !== 'number' || typeof(second) !== 'number' ){
+        result ="ERROR";
+    }else if(first>=0 && second>=0){
+        if(first!=second){
+            for(let i=0; i<=Math.max(first,second); i++){
+                result +=i;
+            }
+        }else{
+            result = first;
+        }
+    }else{
+        result ="ERROR";
+    }
+    return result;
 };
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
-};
-
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const add = function (numA, numB) {
+  return numA + numB;
 
 };
 
-const power = function() {
-	
+const subtract = function (numA, numB) {
+  return numA - numB;
 };
 
-const factorial = function() {
-	
+const sum = function (array) {
+  let result = 0;
+  if (array.length != 0) {
+    for (let element of array) {
+      result += element;
+    }
+  }
+  return result;
+};
+
+const multiply = function (array) {
+  let result = 0;
+  if (array.length > 1) {
+    result = 1;
+    for (let element of array) {
+      result *= element;
+    }
+  }
+  return result;
+};
+
+const power = function (base, exponent) {
+  return Math.pow(base, exponent);
+};
+
+const factorial = function (numA) {
+  let result = 1;
+  for (let i=1; i<=numA;i++){
+    result *=i;
+  }
+  return result;
 };
 
 // Do not edit below this line

aff2bc08172abb9d565504faa878496c2ad239ac
jkmazzetti <jkmazzetti@outlook.com>
2022-01-27 15:42:15 -0300

    Exercises solved.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function (string, times) {
+    let newString = "";
+    if(times>=0){
+    for (let i = 0; i < times; i++) newString += string;
+    }else{
+        newString = "ERROR";
+    }
+    return newString;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,24 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function (array) {
+    let elementsToDelete = Array.prototype.slice.call(arguments);
+    for (let element of elementsToDelete) {
+        console.log(element);
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] === element) {
+                array.splice(i, 1);
+            }
+        }
+    }
+    console.log(array);
+    return array;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+/*
+What's the difference between == and ===?
+
+1=="1" is true, while 1==="1" is false.
+
+*/
